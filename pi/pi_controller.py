76b10cc33aca764ed5bd0184e881858631ef943e
hannaarendt <144813204+hannaarendt@users.noreply.github.com>
2023-11-24 08:20:11 +0100

    Update pi_controller.py

--- pi/pi_controller.py
+++ pi/pi_controller.py
@@ -2,40 +2,39 @@ import requests
 import time
 import random
 import click
+from sense_hat import SenseHat
 
+sense = SenseHat()
 
-# Replace with your own function in Part 1
 def get_direction():
     d_long = 0
     d_la = 0
     send_vel = False
-    c = click.getchar()
-    if c =='a':
-        click.echo('Left')
-        send_vel = True
-        d_long = -1
-        d_la = 0
-    elif c == 'd':
-        click.echo('Right')
-        send_vel = True
-        d_long = 1
-        d_la = 0
-    elif c =='w':
-        click.echo('Up')
-        send_vel = True
-        d_long = 0
-        d_la = 1
-    elif c == 's':
-        click.echo('Down')
-        send_vel = True
-        d_long = 0
-        d_la = -1
-    else:
-        d_long = 0
-        d_la = 0
-        click.echo('Invalid input :(')
-        send_vel = False
+    for event in sense.stick.get_events():
+        if event.action == "pressed":
+
+            if event.direction == "up":
+                print('Up')
+                send_vel = True
+                d_long = 0
+                d_la = 1
+            elif event.direction == "down":
+                print('Down')
+                send_vel = True
+                d_long = 0
+                d_la = -1
+            elif event.direction == "left":
+                print('Left')
+                send_vel = True
+                d_long = -1
+                d_la = 0
+            elif event.direction == "right":
+                print('Right')
+                send_vel = True
+                d_long = 1
+                d_la = 0
     return d_long, d_la, send_vel
+
 
 
 if __name__ == "__main__":
